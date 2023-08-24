c2a8d2fc550c9a94931051c0c2da9a9b5d010153
Adithya <adikrishna1972@gmail.com>
2023-08-24 23:24:02 +0530

    Added the routes enabling authorized users to delete or fetch other user/user's details

--- netflix_server/routes/users.js
+++ netflix_server/routes/users.js
@@ -14,7 +14,6 @@ router.put("/:id", verify, async (req, res) => {
         try {
 
             const updateUser = await User.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
-
             res.status(200).json(updateUser);
 
         } catch (err) {
@@ -27,8 +26,55 @@ router.put("/:id", verify, async (req, res) => {
 })
 
 //DELETE
+router.delete("/:id", verify, async (req, res) => {
+
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+        try {
+
+            await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("User has been deleted successfully!!");
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You can delete only your account!!")
+    }
+
+})
+
 //GET ONE USER
+router.get("/find/:id", async (req, res) => {
+
+        try {
+            const user = await User.findById(req.params.id);
+            const {password, ...info} = user._doc;
+            res.status(200).json(info);
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+)
+
 //GET ALL USERS
+router.get("/", verify, async (req, res) => {
+
+    const query = req.query.new;
+    if (req.user.isAdmin) {
+        try {
+            const users = query ? await User.find(req.params.id).sort({id:-1}).limit(10) : await User.find(req.params.id);
+            res.status(200).json(users);
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json("You are not permitted to view the User's List!!")
+    }
+
+})
+
 //GET USER STATS
 
 module.exports = router;
